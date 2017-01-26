cf5cab90b419901b017b1dcbfdf2769e510348d7
Lars Heuer <lars.heuer@uni-oldenburg.de>
2017-01-26 14:23:54 +0100

    Delete storage class

--- printjob.php
+++ printjob.php
@@ -2,13 +2,14 @@
 // Print book labels
 include 'classes/autoload.php';
 
-$DEBUG = true;
+$DEBUG = false;
 
-if($DEBUG) $yesterday = date("Y-m-d", strtotime("-1 day"));
-else $yesterday = date("Y-m-d", strtotime("2016-09-02 -1 day"));
+$yesterday = date("Y-m-d", strtotime("-1 day"));
 
+// Get newly inventoried items from Alma Analytics
 $ipd = new AlmaInventoryPerDate($yesterday);
 
+// Get List of ZPL print commands
 $book_label_list = new BookLabelList($ipd->GetBarcodes());
 
 $zebra_printer = "ZEBRA_PRINTER";
@@ -17,15 +18,21 @@ $debug_printer = "DEBUG_PRINTER";
 if($DEBUG) $printer = $debug_printer;
 else $printer = $zebra_printer;
 
+// Print the labels via CUPS
 $printjob = new PrintJob($book_label_list, $printer);
 $feedback = $printjob->Execute();
 
-if(!$printjob->Failed())
+// Save printjob to database
+if(Config::Get("SAVE_PRINTJOBS"))
 {
-	foreach($book_label_list->GetItems() as $label)
+	if(!$printjob->Failed())
 	{
-		BookLabelStorage::Save($label, $feedback);
+		foreach($book_label_list->GetItems() as $label)
+		{
+			BookLabelStorage::Save($label, $feedback);
+		}
 	}
 }
+
 
 ?>
